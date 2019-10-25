8168318ab6ac07d995528d4bcbaa1eaac06e80b0
yungik <yungik@glocalizeinc.com>
2019-10-26 01:11:22 +0900

    add prod

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -6,3 +6,11 @@ from backend.settings.base import *
 #         'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.prod.json'),
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR+'/../', 'db.sqlite3'),
+    }
+}
+
+ALLOWED_HOSTS = ['*']
